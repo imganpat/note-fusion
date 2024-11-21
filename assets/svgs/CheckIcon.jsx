9bfb5e1bff70b266d549349d1618c25bd9ae24f9
Ganpat <coolganpat41@gmail.com>
2024-11-21 11:45:43 +0530

    deployment of frontent supporting authentication and authorization

---
+++ assets/svgs/CheckIcon.jsx
@@ -0,0 +1,20 @@
+const CheckIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width={16}
+    height={16}
+    color={"#eff6ff"}
+    fill={"none"}
+  >
+    <path
+      d="M5 14.5C5 14.5 6.5 14.5 8.5 18C8.5 18 14.0588 8.83333 19 7"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export default CheckIcon;
