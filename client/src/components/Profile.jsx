e8436d74acea0f5416854303282957ca23eaae92
Ganpat <coolganpat41@gmail.com>
2025-02-06 12:58:27 +0530

    deployment after impoving performance

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -2,15 +2,8 @@ const Profile = () => {
   const username = localStorage.getItem("username");
   const email = localStorage.getItem("email");
   return (
-    // <div className="flex items-center justify-center bg-red-400 text-3xl">
-    //   hello
-    // </div>
-
     <div className="flex h-full flex-1 flex-col items-center justify-center">
-      {/* <div className="rounded-lg bg-red-400 p-5 text-3xl"> */}
-      <h2 className="text-lg">
-        Hello, {username.toUpperCase()} {/* </div> */}
-      </h2>
+      <h2 className="text-lg">Hello, {username.toUpperCase()}</h2>
     </div>
   );
 };
