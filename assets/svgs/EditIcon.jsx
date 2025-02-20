0809e2668ff57946a5ac7040ce3d55371ee59a6d
Ganpat <coolganpat41@gmail.com>
2025-02-20 20:04:01 +0530

    fixed paste problem while updating the note and added share functionality

--- assets/svgs/EditIcon.jsx
+++ assets/svgs/EditIcon.jsx
@@ -2,8 +2,8 @@ const EditIcon = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24"
-    width={16}
-    height={16}
+    width={15}
+    height={15}
     color={"#eff6ff"}
     fill={"none"}
   >
