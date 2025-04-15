9025df6fbb9f88a2748c2e199269aae44909c698
Ganpat <pganpatchoudhary@gmail.com>
2025-04-15 22:22:54 +0530

    added extraReduceres to update the global state with asynchronous operations

--- client/src/store/slices/notes_slice.js
+++ client/src/store/slices/notes_slice.js
@@ -1,8 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import axios from "axios";
-import backendUrl from "../../constants/backend_url";
-
-const URL = `${backendUrl}/api/notes`;
+import { addNewNote, deleteNote, editNote, getAllNotes, toggleCompletion, toggleImportance } from "../thunks/notes_thunk";
 
 const initialState = {
   notes: [],
@@ -17,86 +14,8 @@ const NoteSlice = createSlice({
   name: "notes",
   initialState,
   reducers: {
-    initNotes(state, action) {
-      state.notes.push(...action.payload);
-    },
-
     clearAllNotes(state) {
       state.notes = [];
-    },
-
-    addNewNote(state, action) {
-      state.notes.push(action.payload);
-      axios.post(`${URL}/add`, action.payload, {
-        withCredentials: true,
-        headers: {
-          Authorization: localStorage.getItem("token"),
-        },
-      });
-    },
-
-    toogleImportance({ notes }, action) {
-      const note = notes.find((note) => note.uid === action.payload.uid);
-      if (note) note.is_important = note.is_important ? 0 : 1; // Coverting True to 1 and false to 0
-
-      axios.put(`${URL}/imp/${action.payload.uid}`, action.payload, {
-        withCredentials: true,
-        headers: {
-          Authorization: localStorage.getItem("token"),
-        },
-      });
-    },
-
-    toogleCompletion(state, action) {
-      const note = state.notes.find((note) => note.uid === action.payload.uid);
-      if (note) note.is_complete = note.is_complete ? 0 : 1; // Coverting True to 1 and false to 0
-
-      axios.put(`${URL}/complete/${action.payload.uid}`, action.payload, {
-        withCredentials: true,
-        headers: {
-          Authorization: localStorage.getItem("token"),
-        },
-      });
-    },
-
-    editNote(state, action) {
-      const currentUid = action.payload.uid;
-      const newDesc = action.payload.description;
-      const newImp = action.payload.is_important;
-      const username = action.payload.username;
-
-      const note = state.notes.map((eachNote) => {
-        if (eachNote.uid === currentUid) {
-          eachNote.description = newDesc;
-          eachNote.is_important = newImp;
-        }
-      });
-
-      axios.put(
-        `${URL}/edit/${action.payload.uid}`,
-        {
-          description: newDesc,
-          is_important: newImp,
-          username: username,
-        },
-        {
-          withCredentials: true,
-          headers: {
-            Authorization: localStorage.getItem("token"),
-          },
-        }
-      );
-    },
-
-    deleteNote(state, action) {
-      state.notes.filter((note) => note.uid !== action.payload.uid);
-
-      axios.delete(`${URL}/delete/${action.payload.uid}`, {
-        withCredentials: true,
-        headers: {
-          Authorization: localStorage.getItem("token"),
-        },
-      });
     },
 
     markAnimated: (state) => {
@@ -107,18 +26,51 @@ const NoteSlice = createSlice({
       state.isLoading = action.payload;
     },
   },
+
+  extraReducers: (builder) => {
+    builder.addCase(getAllNotes.fulfilled, (state, action) => {
+      // action.payload is the array of notes fetched from the server
+      state.notes = action.payload;
+    })
+
+    builder.addCase(addNewNote.fulfilled, (state, action) => {
+      // action.payload is the note object created in the server
+      state.notes.push(action.payload);
+    });
+
+    builder.addCase(editNote.fulfilled, (state, action) => {
+      // action.payload is the note object with updated values
+      const note = state.notes.find((note) => note.uid === action.payload.uid);
+      if (note) {
+        note.description = action.payload.description;
+        note.is_important = action.payload.is_important;
+        note.is_complete = action.payload.is_complete;
+      }
+    });
+
+    builder.addCase(toggleImportance.fulfilled, (state, action) => {
+      // action.payload is the uid of the note to be toggled
+      const note = state.notes.find((note) => note.uid === action.payload);
+      if (note) note.is_important = !note.is_important;
+    });
+
+    builder.addCase(toggleCompletion.fulfilled, (state, action) => {
+      const note = state.notes.find((note) => note.uid === action.payload);
+      if (note) note.is_complete = !note.is_complete;
+    });
+
+    builder.addCase(deleteNote.fulfilled, (state, action) => {
+      // action.payload is the uid of the note to be deleted
+      state.notes = state.notes.filter((note) => note.uid !== action.payload);
+    });
+
+  }
 });
 
 export default NoteSlice;
 
 export const {
-  initNotes,
   clearAllNotes,
-  addNewNote,
-  toogleImportance,
-  toogleCompletion,
-  editNote,
-  deleteNote,
   markAnimated,
   setLoading
 } = NoteSlice.actions;
