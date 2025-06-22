5c90f027aa5bdb952f3f9321612f4e61af6bb668
Ganpat <pganpatchoudhary@gmail.com>
2025-06-22 14:13:58 +0530

    added a function to get scookie

--- client/src/lib/utils.js
+++ client/src/lib/utils.js
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+export function getCookie(name) {
+  const match = document.cookie.match(new RegExp("(^| )" + name + "=([^;]+)"));
+  return match ? match[2] : null;
+}
