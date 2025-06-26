9bca4747abf39cd5bd402179c6867c0f3bdf8027
Ganpat <pganpatchoudhary@gmail.com>
2025-06-26 22:17:08 +0530

    integrated and optimised the edit note functionality

--- client/src/components/InputPopup.jsx
+++ client/src/components/InputPopup.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { addNewNote, editNote } from "../store/thunks/notes_thunk.js";
 import {
   Dialog,
@@ -7,33 +7,34 @@ import {
   DialogDescription,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from "./ui/dialog.jsx";
 import { Textarea } from "./ui/textarea.jsx";
 import { Input } from "./ui/input.jsx";
 import { Checkbox } from "./ui/checkbox.jsx";
 import { Label } from "./ui/label.jsx";
 import { Button } from "./ui/button.jsx";
-import { Loader2Icon } from "lucide-react";
+import { Loader2Icon, Plus } from "lucide-react";
+import { useInputPopup } from "@/context/InputPopupContext.jsx";
 
 const handleAddOrUpdateNote = async (
-  setOpen,
   setLoading,
   dispatch,
-  isEditing,
+  mode,
   currentNote,
   title,
   description,
   is_important,
   is_completed,
-  username
+  username,
+  closePopup,
+  isEditing = mode == "edit" || false
 ) => {
   const date = new Date();
   const noteDate = date.toISOString().slice(0, 19).replace("T", " ");
 
   const note = {
     uid: isEditing ? currentNote.uid : null, // If editing, keep the current uid else set to null
-    title, // If editing, keep the current title else set to "New Note"
+    title,
     description,
     created_at: isEditing ? currentNote.created_at : noteDate, // If editing, keep the current created_at date else set the new date
     is_important,
@@ -44,8 +45,8 @@ const handleAddOrUpdateNote = async (
   setLoading(true);
   if (isEditing) await dispatch(editNote(note));
   else await dispatch(addNewNote(note));
-  setOpen(false);
   setLoading(false);
+  closePopup();
 };
 
 const InputPopup = () => {
@@ -54,31 +55,18 @@ const InputPopup = () => {
   const titleRef = useRef();
   const descriptionRef = useRef();
 
-  const { isEditing, currentNote } = useSelector((state) => state.popup); // getting isEditing and currentNote from popup slice
+  const { open, setOpen, mode, openPopup, currentNote, closePopup } =
+    useInputPopup(); // getting isEditing and currentNote from popup slice
 
-  const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
-
-  const [title, setTitle] = useState(
-    // setting title to currentNote title if editing else empty string
-    currentNote?.title || ""
-  );
-
-  const [description, setDescription] = useState(
-    // setting description to currentNote description if editing else empty string
-    currentNote?.description || ""
-  );
-
-  const [is_important, setIsImportant] = useState(
-    currentNote?.is_important || false // setting is_important to currentNote is_important if editing else false
-  );
-  const [is_completed, setIsCompleted] = useState(
-    currentNote?.is_completed || false // setting is_completed to currentNote is_completed if editing else false
-  );
-  const username = localStorage.getItem("username"); // getting username from local storage
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [is_important, setIsImportant] = useState(false);
+  const [is_completed, setIsCompleted] = useState(false);
+  const username = localStorage.getItem("username");
 
   useEffect(() => {
-    if (isEditing) {
+    if (mode === "edit") {
       setTitle(currentNote.title);
       setDescription(currentNote.description);
       setIsImportant(currentNote.is_important);
@@ -91,7 +79,7 @@ const InputPopup = () => {
         descriptionRef.current.value.length
       );
     }
-  }, [isEditing, currentNote]);
+  }, [currentNote]);
 
   //clearing the previous values when the open state changes
   useEffect(() => {
@@ -104,74 +92,82 @@ const InputPopup = () => {
   }, [open]);
 
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogTrigger className="inline-flex h-10 w-full items-center justify-center gap-2 whitespace-nowrap rounded-md bg-primary text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0">
-        Add a Note
-      </DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle className="sr-only">Add a new Note</DialogTitle>
-          <DialogDescription>
-            <Input
-              placeholder="New Title"
-              className="!focus:outline-none !focus:ring-0 !focus:border-none !border-none px-0 !text-lg font-semibold !outline-none !ring-0"
-              ref={titleRef}
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-            />
-            <Textarea
-              placeholder="Enter your note here"
-              className="!focus:outline-none !focus:ring-0 !focus:border-none min-h-40 !border-none px-0 !text-base !outline-none !ring-0"
-              ref={descriptionRef}
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-            />
-
-            <span className="my-4 flex items-center space-x-2">
-              <Checkbox
-                id="important"
-                disabled={loading || (!title.trim() && !description.trim())} // Disable checkbot if the on loading and if title and description are empty
-                onChange={() => setIsImportant(!is_important)}
+    <>
+      <Button
+        className="fixed bottom-4 right-4 z-50 inline-flex h-10 w-10 items-center justify-center rounded-full bg-primary text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 sm:static sm:w-full sm:gap-2 sm:whitespace-nowrap sm:rounded-md sm:text-sm sm:font-medium sm:[&_svg]:pointer-events-none sm:[&_svg]:size-4 sm:[&_svg]:shrink-0"
+        onClick={() => openPopup({ mode: "add", currentNote: null })}
+      >
+        <Plus className="size-4 shrink-0" />
+        <span className="hidden sm:inline">Add Note</span>
+      </Button>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle className="sr-only">
+              {mode === "edit" ? "Edit Note" : "Add Note"}
+            </DialogTitle>
+            <DialogDescription>
+              <Input
+                placeholder="New Title"
+                className="!focus:outline-none !focus:ring-0 !focus:border-none !border-none px-0 !text-lg font-semibold !outline-none !ring-0"
+                ref={titleRef}
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
               />
-              <Label htmlFor="important" className="cursor-pointer">
-                Mark important
-              </Label>
-            </span>
-            <Button
-              className="h-10 w-full px-8"
-              disabled={loading || (!title.trim() && !description.trim())} // Disabling the button
-              onClick={async () => {
-                await handleAddOrUpdateNote(
-                  setOpen,
-                  setLoading,
-                  dispatch,
-                  isEditing,
-                  currentNote,
-                  title,
-                  description,
-                  is_important,
-                  is_completed,
-                  username
-                );
-              }}
-            >
-              {loading ? (
-                <span className="flex items-center gap-1">
-                  {/* display loading spinner along with the operation performed message */}
-                  <Loader2Icon className="animate-spin" />
-                  {isEditing ? "Editing..." : "Adding..."}
-                </span>
-              ) : isEditing ? (
-                // changing the button tect based on the operation
-                "Update"
-              ) : (
-                "Add"
-              )}
-            </Button>
-          </DialogDescription>
-        </DialogHeader>
-      </DialogContent>
-    </Dialog>
+              <Textarea
+                placeholder="Enter your note here"
+                className="!focus:outline-none !focus:ring-0 !focus:border-none min-h-40 !border-none px-0 !text-base !outline-none !ring-0"
+                ref={descriptionRef}
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              />
+
+              <span className="my-4 flex items-center space-x-2">
+                <Checkbox
+                  id="important"
+                  disabled={loading || (!title.trim() && !description.trim())} // Disable checkbot if the on loading and if title and description are empty
+                  onChange={() => setIsImportant(!is_important)}
+                />
+                <Label htmlFor="important" className="cursor-pointer">
+                  Mark important
+                </Label>
+              </span>
+              <Button
+                className="h-10 w-full px-8"
+                disabled={loading || (!title.trim() && !description.trim())} // Disabling the button
+                onClick={async () => {
+                  await handleAddOrUpdateNote(
+                    setLoading,
+                    dispatch,
+                    mode,
+                    currentNote,
+                    title,
+                    description,
+                    is_important,
+                    is_completed,
+                    username,
+                    closePopup
+                  );
+                }}
+              >
+                {loading ? (
+                  <span className="flex items-center gap-1">
+                    {/* display loading spinner along with the operation performed message */}
+                    <Loader2Icon className="animate-spin" />
+                    {mode === "edit" ? "Updating..." : "Adding..."}
+                  </span>
+                ) : mode === "edit" ? (
+                  // changing the button tect based on the operation
+                  "Update"
+                ) : (
+                  "Add"
+                )}
+              </Button>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
 
