874e60816612a83998522ea9d446f6e75a60b7cc
Ganpat <pganpatchoudhary@gmail.com>
2025-06-27 22:50:28 +0530

    changed the url form local to remote server

--- client/src/constants/backend_url.js
+++ client/src/constants/backend_url.js
@@ -1,4 +1,4 @@
-// const backendUrl = "https://note-fusion.onrender.com"
-const backendUrl = "http://localhost:3000"
+const backendUrl = "https://note-fusion.onrender.com"
+// const backendUrl = "http://localhost:3000"
 
 export default backendUrl;
